346d01147666ccff2d20a2a1d3cf0b760fe81e0c
Michael Dawson <m.dawson@y7mail.com>
2025-03-30 14:31:12 -0400

    Add date-fns packages

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,1 +1,9 @@
 export const DATE_FORMAT = "yyyy-MM-dd";
+
+import { format } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
+
+// Get a date string that can be used for SQL queries
+export function getDateString(date: Date, timezone: string) {
+  return format(toZonedTime(date, timezone), DATE_FORMAT);
+}
