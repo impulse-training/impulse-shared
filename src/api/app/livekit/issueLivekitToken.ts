3513db9672dd8b6101c034fd3f3807a914fa5b66
Michael Dawson <m.dawson@y7mail.com>
2025-04-16 12:52:36 -0500

    Update API signature

--- src/api/app/livekit/issueLivekitToken.ts
+++ src/api/app/livekit/issueLivekitToken.ts
@@ -1,9 +1,14 @@
 export type IssueLivekitTokenRequestBody = {
+  // In-app calls with Zara
   threadDocPath?: string;
+
+  // Sessions app calls with humans
+  sessionDocPath?: string;
 };
 
 export type IssueLivekitTokenResponseBody = {
   token: string;
   callLogDocPath: string;
   expiresInMs: number;
+  roomName: string;
 };
--- src/schemas/session.ts
+++ src/schemas/session.ts
@@ -3,9 +3,11 @@ import { timestampSchema } from "../utils";
 
 export const sessionSchema = yup.object({
   participantUids: yup.array().of(yup.string()).required(),
-  timestamp: timestampSchema,
+  startTime: timestampSchema,
+  durationMinutes: yup.number().required(),
   title: yup.string(),
   transcriptSummary: yup.string(),
+  code: yup.string().required(),
 });
 
 export type Session = yup.InferType<typeof sessionSchema>;
