22a85237433014b8dc6d92aea476f3c85614c749
Michael Dawson <m.dawson@y7mail.com>
2025-04-30 10:44:14 -0500

    Clean up build artifacts

--- src/schemas/thread/impulse.ts
+++ src/schemas/thread/impulse.ts
@@ -1,13 +1,11 @@
 import * as yup from "yup";
 import { timestampSchema } from "../../utils";
-import { outcomeSchema } from "../../utils/outcomes";
 import { threadBaseSchema } from "./base";
 
 export const impulseThreadSchema = threadBaseSchema.shape({
   type: yup.mixed<"impulse">().oneOf(["impulse"]).required(),
   behaviorId: yup.string().defined().nullable(),
   debriefedAt: timestampSchema,
-  outcome: outcomeSchema,
 });
 
 export type ImpulseThread = yup.InferType<typeof impulseThreadSchema>;
