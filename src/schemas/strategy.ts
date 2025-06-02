aa6d1f51673577a140d8b911c57d59cb77b79be3
Michael Dawson <m.dawson@y7mail.com>
2025-06-02 10:16:53 -0600

    Add id to strategy type

--- src/schemas/strategy.ts
+++ src/schemas/strategy.ts
@@ -5,7 +5,9 @@ import { gameplanSchema } from "./gameplan";
 import { routineSchema } from "./routine";
 
 export const strategySchema = yup.object({
+  id: yup.string(),
   name: yup.string().required(),
+  description: yup.string(),
   gameplans: yup.array().of(gameplanSchema).required(),
   routines: yup.array().of(routineSchema).required(),
   folders: yup.array().of(folderSchema).required(),
