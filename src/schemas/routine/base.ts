fb1d2c9c4cb5cbce98a8456660dd2803071bef43
Michael Dawson <m.dawson@y7mail.com>
2025-07-07 10:51:25 -0600

    Add properties to routine base

--- src/schemas/routine/base.ts
+++ src/schemas/routine/base.ts
@@ -8,6 +8,8 @@ export function routineBaseSchema<T extends string>(type: T) {
     type: yup.mixed<T>().oneOf([type]).required(),
     ordinal: yup.number(),
     // items can be tactics or folders
+    isLocked: yup.boolean().optional().default(undefined),
+    isTemplate: yup.boolean().optional().default(undefined),
     items: yup.array().of(documentReferenceSchema.required()).required(),
     lastUsedAt: timestampSchema,
     createdAt: timestampSchema,
