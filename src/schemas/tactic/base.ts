6a2bb3fb1e92ca0f348b56ea37cd3120e55516f4
Michael Dawson <m.dawson@y7mail.com>
2025-07-17 11:34:19 -0500

    Remove tacticCollections from user document

--- src/schemas/tactic/base.ts
+++ src/schemas/tactic/base.ts
@@ -21,8 +21,6 @@ export const tacticBaseSchema = yup.object({
   allBehaviors: yup.boolean().optional(),
   behaviorIds: yup.array().of(yup.string().required()).optional(),
 
-  planIds: yup.array().of(yup.string().required()).optional(),
-
   // The original creator of the tactic
   createdByUid: yup.string(),
 
--- src/schemas/userData.ts
+++ src/schemas/userData.ts
@@ -27,10 +27,6 @@ export const userDataSchema = yup.object({
 
   // This points to the user's active strategy. It can be updated to rollback.
   activeStrategyDoc: documentReferenceSchema,
-  tacticCollections: yup
-    .array()
-    .of(documentReferenceSchema.required())
-    .required(),
 
   // User preferences
   theme: yup
