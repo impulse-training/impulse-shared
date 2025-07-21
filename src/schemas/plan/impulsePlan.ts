8290d1cff69d9d982650cd2511821a7b51c37ca8
Michael Dawson <m.dawson@y7mail.com>
2025-07-21 18:48:25 -0500

    Add optional trigger keywords to impulse plans

--- src/schemas/plan/impulsePlan.ts
+++ src/schemas/plan/impulsePlan.ts
@@ -1,6 +1,8 @@
 import * as yup from "yup";
 import { planBaseSchema } from "./base";
 
-export const impulsePlanSchema = planBaseSchema("impulse");
+export const impulsePlanSchema = planBaseSchema("impulse").shape({
+  triggerKeywords: yup.array().of(yup.string().required()).optional(),
+});
 
 export type ImpulsePlan = yup.InferType<typeof impulsePlanSchema>;
