55d886d18d7335f10cca6ff32cbcd6132415af1d
Michael Dawson <m.dawson@y7mail.com>
2025-08-01 09:43:37 -0700

    Add repeat count to affirmations

--- src/schemas/tactic/affirmation.ts
+++ src/schemas/tactic/affirmation.ts
@@ -3,6 +3,8 @@ import { tacticBaseSchema } from "./base";
 
 export const affirmationTacticSchema = tacticBaseSchema.shape({
   type: yup.string().oneOf(["affirmation"]).required(),
+  repeatCount: yup.number().required(),
+  affirmationText: yup.string().required(),
 });
 
 export type AffirmationTactic = yup.InferType<typeof affirmationTacticSchema>;
