0149fc606eed8874218c341dda0b4528fce082f3
Michael Dawson <m.dawson@y7mail.com>
2025-08-29 19:01:39 -0700

    Remove timestamps from image schema

--- src/defaultImages.ts
+++ src/defaultImages.ts
@@ -4,31 +4,76 @@
  */
 
 import { Attachment } from "./schemas";
-import { Timestamp } from "./types";
-
-const nowTs: Timestamp = { toDate: () => new Date(), toMillis: () => Date.now(), seconds: Math.floor(Date.now()/1000), nanoseconds: 0, isEqual: () => false, valueOf: () => "" } as Timestamp;
 
 const baseImages = [
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fendless-constellation.png?alt=media&token=a4295eea-806b-4d39-ac5c-bf6c0bc7602e", storagePath: "tactics/endless-constellation.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fflat-mountains.png?alt=media&token=0e9a148b-d7cf-47e1-9fb8-32e1424c45cd", storagePath: "tactics/flat-mountains.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fhollowed-boxes.png?alt=media&token=a7cfaac5-02b3-4b6d-894e-98a56ca72474", storagePath: "tactics/hollowed-boxes.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fliquid-cheese.png?alt=media&token=6ac8285a-dd0e-492f-9d3a-c6fa2f4bd579", storagePath: "tactics/liquid-cheese.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fquantum-gradient.png?alt=media&token=908b6904-b144-4f55-b2ce-2cd2c99b1f4b", storagePath: "tactics/quantum-gradient.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fradiant-gradient.png?alt=media&token=4a89c2b2-be4d-404f-b747-28be891d7adf", storagePath: "tactics/radiant-gradient.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Frainbow-vortex.png?alt=media&token=7fa62fd7-cc50-47ec-8e54-d9d43fdae010", storagePath: "tactics/rainbow-vortex.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Frose-petals.png?alt=media&token=286a66b0-cd93-4f39-a590-d09c854e0649", storagePath: "tactics/rose-petals.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fscattered-forcefields.png?alt=media&token=f1f13631-86a2-4fb0-8c43-476183bd694c", storagePath: "tactics/scattered-forcefields.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fslanted-gradient.png?alt=media&token=8725ecab-52f2-4dae-b3d5-0dd67e4ef599", storagePath: "tactics/slanted-gradient.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fsubtle-prism.png?alt=media&token=d7ac88b9-cae7-42ef-8ea1-e759eb02544c", storagePath: "tactics/subtle-prism.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fsun-tornado.png?alt=media&token=abacf3bd-e8fd-470c-b1df-8003ed1f7dbb", storagePath: "tactics/sun-tornado.png", contentType: "image/png" },
-  { uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fwintery-sunburst.png?alt=media&token=8858ca04-9a04-41f5-a10b-870e93f569fe", storagePath: "tactics/wintery-sunburst.png", contentType: "image/png" },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fendless-constellation.png?alt=media&token=a4295eea-806b-4d39-ac5c-bf6c0bc7602e",
+    storagePath: "tactics/endless-constellation.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fflat-mountains.png?alt=media&token=0e9a148b-d7cf-47e1-9fb8-32e1424c45cd",
+    storagePath: "tactics/flat-mountains.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fhollowed-boxes.png?alt=media&token=a7cfaac5-02b3-4b6d-894e-98a56ca72474",
+    storagePath: "tactics/hollowed-boxes.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fliquid-cheese.png?alt=media&token=6ac8285a-dd0e-492f-9d3a-c6fa2f4bd579",
+    storagePath: "tactics/liquid-cheese.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fquantum-gradient.png?alt=media&token=908b6904-b144-4f55-b2ce-2cd2c99b1f4b",
+    storagePath: "tactics/quantum-gradient.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fradiant-gradient.png?alt=media&token=4a89c2b2-be4d-404f-b747-28be891d7adf",
+    storagePath: "tactics/radiant-gradient.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Frainbow-vortex.png?alt=media&token=7fa62fd7-cc50-47ec-8e54-d9d43fdae010",
+    storagePath: "tactics/rainbow-vortex.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Frose-petals.png?alt=media&token=286a66b0-cd93-4f39-a590-d09c854e0649",
+    storagePath: "tactics/rose-petals.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fscattered-forcefields.png?alt=media&token=f1f13631-86a2-4fb0-8c43-476183bd694c",
+    storagePath: "tactics/scattered-forcefields.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fslanted-gradient.png?alt=media&token=8725ecab-52f2-4dae-b3d5-0dd67e4ef599",
+    storagePath: "tactics/slanted-gradient.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fsubtle-prism.png?alt=media&token=d7ac88b9-cae7-42ef-8ea1-e759eb02544c",
+    storagePath: "tactics/subtle-prism.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fsun-tornado.png?alt=media&token=abacf3bd-e8fd-470c-b1df-8003ed1f7dbb",
+    storagePath: "tactics/sun-tornado.png",
+    contentType: "image/png",
+  },
+  {
+    uri: "https://firebasestorage.googleapis.com/v0/b/impulse-tmp.appspot.com/o/tactics%2Fwintery-sunburst.png?alt=media&token=8858ca04-9a04-41f5-a10b-870e93f569fe",
+    storagePath: "tactics/wintery-sunburst.png",
+    contentType: "image/png",
+  },
 ] as const;
 
-export const defaultImages: Attachment[] = baseImages.map((img) => ({
-  createdAt: nowTs,
-  updatedAt: nowTs,
-  ...img,
-}));
+export const defaultImages = baseImages;
 
 /**
  * Get a random default image from the available options
