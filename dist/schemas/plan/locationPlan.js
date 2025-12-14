e1ab4bf4e7defbd820ab4cc22babfda920c6b553
Michael Dawson <m.dawson@y7mail.com>
2025-12-14 07:46:36 -0600

    Make location trigger required on location plans

--- dist/schemas/plan/locationPlan.js
+++ dist/schemas/plan/locationPlan.js
@@ -4,5 +4,5 @@ exports.locationPlanSchema = void 0;
 const base_1 = require("./base");
 const locationTrigger_1 = require("./trigger/locationTrigger");
 exports.locationPlanSchema = (0, base_1.planBaseSchema)("location").extend({
-    trigger: locationTrigger_1.locationTriggerSchema,
+    trigger: locationTrigger_1.locationTriggerSchema.required(),
 });
--- src/schemas/plan/locationPlan.ts
+++ src/schemas/plan/locationPlan.ts
@@ -3,7 +3,7 @@ import { planBaseSchema } from "./base";
 import { locationTriggerSchema } from "./trigger/locationTrigger";
 
 export const locationPlanSchema = planBaseSchema("location").extend({
-  trigger: locationTriggerSchema,
+  trigger: locationTriggerSchema.required(),
 });
 
 export type LocationPlan = z.infer<typeof locationPlanSchema>;
