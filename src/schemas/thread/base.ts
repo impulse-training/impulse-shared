65547c1f92026dd811736b80bf20722fb3d8fdcf
Michael Dawson <m.dawson@y7mail.com>
2025-12-30 11:38:44 -0600

    Remove plan attribute from thread schema - planId will be passed as route param instead

--- src/schemas/thread/base.ts
+++ src/schemas/thread/base.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 import { documentReferenceSchema } from "../../utils/documentReferenceSchema";
 import { timestampSchema } from "../../utils/timestampSchema";
 import { emojiIdSchema } from "../emojiId";
-import { planWithIdSchema } from "../plan";
 import { tacticSchema } from "../tactic/tactic";
 import { threadSummarySchema } from "../threadSummary";
 
@@ -23,8 +22,6 @@ export const threadBaseSchema = z.object({
   id: z.string().optional(),
   // Optional title for the thread (e.g., plan name)
   title: z.string().optional(),
-  // Any thread may have an optional plan
-  plan: planWithIdSchema.optional(),
   type: threadTypeSchema.default("general"),
   date: timestampSchema,
   dateString: z.string(),
