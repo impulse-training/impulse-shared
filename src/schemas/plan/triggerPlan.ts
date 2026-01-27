17005ca26ac42747b439c599ef60aaf8c9e59e8e
Michael Dawson <m.dawson@y7mail.com>
2026-01-27 09:13:40 -0600

    Remove unused plan stats properties from triggerPlan schema

--- src/schemas/plan/triggerPlan.ts
+++ src/schemas/plan/triggerPlan.ts
@@ -1,12 +1,7 @@
 import { z } from "zod";
-import { timestampSchema } from "../../utils/timestampSchema";
 import { planBaseSchema } from "./base";
 
 export const triggerPlanSchema = planBaseSchema("trigger").extend({
-  lastUsedAt: timestampSchema.optional(),
-  numberOfUses: z.number().int().nonnegative().default(0),
-  numberOfSuccesses: z.number().int().nonnegative().default(0),
-  numberOfSetbacks: z.number().int().nonnegative().default(0),
   isActive: z.boolean().optional(),
 });
 
